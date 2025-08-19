7ac6b99a939358e8aa08e8fc6db18c8d2a3e0c14
pubethSri <66070158@kmitl.ac.th>
2025-08-19 15:11:48 +0700

    webapp connect to mongodb can add can delete can query

---
+++ web/app.py
@@ -0,0 +1,47 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask import redirect
+from flask import url_for
+from pymongo import MongoClient
+from bson import ObjectId
+
+sample = Flask(__name__)
+
+data = []
+
+client = MongoClient("mongodb://mongo:27017/")
+mydb = client["router_database"]
+mycol = mydb["router_collection"]
+
+@sample.route("/")
+def main():
+    routers = list(mycol.find({}))
+    for r in routers:
+        r["_id"] = str(r["_id"])
+    return render_template("index.html", data=routers)
+
+@sample.route("/add", methods=["POST"])
+def add_router():
+    ip = request.form.get("ip")
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    if ip and username:
+        data.append({"ip": ip, "username": username})
+        router_to_add = {"ip": ip, "username": username, "password": password}
+        mycol.insert_one(router_to_add)
+    return redirect(url_for("main"))
+
+@sample.route("/delete", methods=["POST"])
+def delete_router():
+    doc_id = request.form.get("id")
+    if doc_id:
+        try:
+            mycol.delete_one({"_id": ObjectId(doc_id)})
+        except Exception as e:
+            print("Delete failed:", e)
+    return redirect(url_for("main"))
+
+if __name__ == "__main__":
+    sample.run(host="0.0.0.0", port=8080)
