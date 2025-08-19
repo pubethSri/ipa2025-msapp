25eb154b4e32584dd41eba3f6c2f6356f2d63d12
pubethSri <66070158@kmitl.ac.th>
2025-08-19 12:07:17 +0700

    add files

--- sample-app-master/sample-app.sh
+++ sample-app-master/sample-app.sh
@@ -4,7 +4,7 @@ mkdir tempdir
 mkdir tempdir/templates
 mkdir tempdir/static
 
-cp sample_app.py tempdir/.
+cp app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
@@ -14,12 +14,12 @@ echo "RUN pip install flask" >> tempdir/Dockerfile
 
 echo "COPY ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
-echo "COPY sample_app.py /home/myapp/" >> tempdir/Dockerfile
+echo "COPY app.py /home/myapp/" >> tempdir/Dockerfile
 
-echo "EXPOSE 5050" >> tempdir/Dockerfile
-echo "CMD python3 /home/myapp/sample_app.py" >> tempdir/Dockerfile
+echo "EXPOSE 8080" >> tempdir/Dockerfile
+echo "CMD python3 /home/myapp/app.py" >> tempdir/Dockerfile
 
 cd tempdir
 docker build -t sampleapp .
-docker run -t -d -p 5050:5050 --name sampletest sampleapp
+docker run -t -d -p 8080:8080 --name sampletest sampleapp
 docker ps -a
